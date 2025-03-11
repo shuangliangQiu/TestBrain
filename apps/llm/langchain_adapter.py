c5dd500e15c687e31ff278d60367584639a54a27
mango <576027405@qq.com>
2025-03-11 11:54:20 +0800

    标记无用的文件

--- apps/llm/langchain_adapter.py
+++ apps/llm/langchain_adapter.py
@@ -1,30 +1,31 @@
-from typing import Any, List, Optional
-from langchain_core.messages import BaseMessage
-from langchain_core.callbacks.manager import CallbackManagerForLLMRun
-from .base import BaseLLMService
+#TODO:确定没有使用后需要删除
+# from typing import Any, List, Optional
+# from langchain_core.messages import BaseMessage
+# from langchain_core.callbacks.manager import CallbackManagerForLLMRun
+# from .base import BaseLLMService
 
-class LangChainAdapter(BaseLLMService):
-    """LangChain适配器"""
+# class LangChainAdapter(BaseLLMService):
+#     """LangChain适配器"""
     
-    def __init__(self, model):
-        """初始化
-        Args:
-            model: LangChain模型实例
-        """
-        self.model = model
-        super().__init__()
+#     def __init__(self, model):
+#         """初始化
+#         Args:
+#             model: LangChain模型实例
+#         """
+#         self.model = model
+#         super().__init__()
 
-    def _generate(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> str:
-        """调用模型生成响应"""
-        response = self.model.invoke(messages)
-        return response
+#     def _generate(
+#         self,
+#         messages: List[BaseMessage],
+#         stop: Optional[List[str]] = None,
+#         run_manager: Optional[CallbackManagerForLLMRun] = None,
+#         **kwargs: Any,
+#     ) -> str:
+#         """调用模型生成响应"""
+#         response = self.model.invoke(messages)
+#         return response
     
-    @property
-    def _llm_type(self) -> str:
-        return "langchain_adapter"
+#     @property
+#     def _llm_type(self) -> str:
+#         return "langchain_adapter"
