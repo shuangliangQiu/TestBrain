4c316a37d1659914c0384f2c61b11b81c6f1ed39
mango <576027405@qq.com>
2025-03-20 14:54:14 +0800

    更新配置

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0',
+]
 
 # 是否启用Milvus
 ENABLE_MILVUS = False
